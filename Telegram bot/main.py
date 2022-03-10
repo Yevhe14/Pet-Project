5ae639615006431f54418cb36b0aba64ecb3bd14
YevhenBilokin <zenyen14@gmail.com>
2022-03-10 18:31:32 +0200

    README

--- Telegram bot/main.py
+++ Telegram bot/main.py
@@ -1,5 +1,19 @@
-from PIL import Image, ImageFont, ImageDraw 
+import logging
 
+import telebot
+from PIL import Image, ImageDraw, ImageFont
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+Token = "5272300324:AAHCJ1VjTvXJ-RbLu5a71zq5u4lHqapDfzk"
+bot = telebot.TeleBot(Token)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+
+#func for add text on photo 
 def print_text_on_photo(image, text, font):
     draw = ImageDraw.Draw(image)
     width_image, height_image = image.size
@@ -12,3 +26,28 @@ def print_text_on_photo(image, text, font):
     )
 
     image.save('Result.jpg')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+	bot.send_message(message.chat.id,'Привет')
+
+
+@bot.message_handler(content_types=['document'])
+def handle_docs_photo(message):
+    try:
+        chat_id = message.chat.id
+
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        src = 'C:/Users/Acer/Desktop/photo/' + message.document.file_name;
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+
+        bot.reply_to(message, "Додається текст")
+    except Exception as e:
+        bot.reply_to(message, "Сталася помилка!\
+            \nСпробуйте знову")
+
+
+bot.infinity_polling()
