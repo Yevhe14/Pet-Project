fcbe9bb8e70037770a25bd36c749a3323d5480d3
YevhenBilokin <zenyen14@gmail.com>
2022-03-10 12:54:59 +0200

    Telegram bot draw on image

--- Telegram bot/main.py
+++ Telegram bot/main.py
@@ -1,13 +1,19 @@
 from PIL import Image, ImageFont, ImageDraw 
 
+
+
 my_image = Image.open("D:\Python GoIteens\Pet-Project\Telegram bot\p1.png")
 
 title_text = "The Beauty of Nature"
 
-image_editable = ImageDraw.Draw(my_image)
-
 title_font = ImageFont.truetype('D:\Python GoIteens\Pet-Project\Telegram bot\PlayfairDisplay-VariableFont_wght.ttf', 100)
 
-image_editable.text((15,15), title_text, (237, 230, 211), font=title_font)
 
-my_image.save("result.jpg")
+def print_text_on_photo(image, text, font_user):
+    image_editable = ImageDraw.Draw(image)
+
+    image_editable.text((15,15), text, (237,230,211), font=font_user)
+
+    image.save('Result.jpg')
+
+print_text_on_photo(my_image, title_text, title_font)
