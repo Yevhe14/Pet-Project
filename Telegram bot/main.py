b5731b0fd7bbef988f500df04347caf4d8d5f831
YevhenBilokin <zenyen14@gmail.com>
2022-03-10 13:11:28 +0200

    Telegram bot draw on image

--- Telegram bot/main.py
+++ Telegram bot/main.py
@@ -6,13 +6,20 @@ my_image = Image.open("D:\Python GoIteens\Pet-Project\Telegram bot\p1.png")
 
 title_text = "PHOTO"
 
-title_font = ImageFont.truetype('D:\Python GoIteens\Pet-Project\Telegram bot\PlayfairDisplay-VariableFont_wght.ttf', 100)
+title_font = ImageFont.truetype('D:\Python GoIteens\Pet-Project\Telegram bot\PlayfairDisplay-VariableFont_wght.ttf', 40)
 
 
-def print_text_on_photo(image, text, font_user):
-    image_editable = ImageDraw.Draw(image)
+def print_text_on_photo(image, text, font):
+    draw = ImageDraw.Draw(image)
 
-    image_editable.text((15,15), text, (237,230,211), font=font_user)
+    width_image, height_image = image.size
+    width_text, height_text = draw.textsize(text, font=font)
+    draw.text(
+        ((width_image - width_text) / 2, ((height_image / 10) * 9)),
+        text,
+        font=font,
+        fill=(0, 0, 0, 0)
+    )
 
     image.save('Result.jpg')
 
