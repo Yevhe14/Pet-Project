3f8897a549ef342344a975ee97fcda21832f59df
YevhenBilokin <zenyen14@gmail.com>
2022-03-12 14:04:51 +0200

    Telegram bot draw on photo

--- Telegram bot/main.py
+++ Telegram bot/main.py
@@ -49,24 +49,6 @@ def get_text_on_photo(message):
     global text
     text = message.text
     bot.send_message(message.chat.id, "Відправте фото")
-'''
-@bot.message_handler(content_types=['document'])
-def handle_docs_photo(message):
-    try:
-        chat_id = message.chat.id
-
-        file_info = bot.get_file(message.document.file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-
-        src = 'C:/Users/Acer/Desktop/photo/' + message.document.file_name;
-        with open(src, 'wb') as new_file:
-            new_file.write(downloaded_file)
-
-        bot.reply_to(message, "Додається текст")
-    except Exception as e:
-        bot.reply_to(message, "Сталася помилка!\
-            \nСпробуйте знову")
-'''
 
 @bot.message_handler(content_types=['photo'])
 def image(message) -> None:
