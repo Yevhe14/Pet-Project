35c0a173838f81bed3da3d6e15a54058859b3d64
YevhenBilokin <zenyen14@gmail.com>
2022-03-18 12:38:53 +0200

    Telegram bot draw on photo

--- Telegram bot/main.py
+++ Telegram bot/main.py
@@ -42,8 +42,6 @@ def start_message(message):
     ru = types.InlineKeyboardButton(text="Руский",callback_data="ru")
     keyboard.add(en,ua,ru)
     bot.send_message(message.chat.id, "Выберете язык(Chouse lenguage):", reply_markup=keyboard)
-    bot.send_message(message.chat.id, 'Выберете язык(Chouse lenguage):')
-
 @bot.message_handler(commands=['photo'])
 def text_on_photo(message):
     bot.send_message(message.chat.id, 'Введіть текст: ')
